98739d614dacd9ee0796ce9d51dc264781d5dad7
Ruslan_Belov <belovruslan15@mail.ru>
2022-12-12 13:58:42 +0700

    Добавьте файлы проекта.

---
+++ Calculator_OOP/Calculator_OOP/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator_OOP
+{
+    class Calculator
+    {
+        private Expression _context;
+        private IOperator _oper;
+       
+        public Calculator(Expression context, IOperator oper)
+        {
+            _context = context;
+            _oper = oper;
+        }
+
+
+    }
+}
---
+++ Calculator_OOP/Calculator_OOP/Operation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator_OOP
+{
+    enum Operation
+    {
+        Addition,
+        Substraction,
+        Multiplication,
+        Division,
+        Pow
+    }
+}
